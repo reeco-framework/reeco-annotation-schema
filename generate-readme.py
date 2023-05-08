2e26dc5bc5e77faf2a7890083a18fd2e44ed7a61
Enrico Daga <enrico.daga@open.ac.uk>
2023-05-08 11:47:50 +0100

    Only show example if there is a non empty value

--- generate-readme.py
+++ generate-readme.py
@@ -94,7 +94,7 @@ for term in SCHEMA.terms():
             TXT = TXT + "*" + k + "*: " + str(term[k]) + "\n"
     if 'description' in term:
         TXT = TXT + "\n" + str(term[ 'description']) + "\n"
-    if 'example-values' in term and term['example-values'] != '':
+    if 'example-values' in term and term['example-values'].strip() != '':
         TXT = TXT + "\n\n```" + str(term[ 'example-values']) + "\n```\n\n"
 
 with open(output_readme, "w") as text_file:
