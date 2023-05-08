1451d3d77d4f5b6c5d8900f5aff16077f41a650f
Enrico Daga <enrico.daga@open.ac.uk>
2023-05-08 11:42:36 +0100

    Use space not tabs in lists

--- generate-readme.py
+++ generate-readme.py
@@ -29,30 +29,30 @@ Container types:
 """
 # List of container types
 for component in SCHEMA.containers():
-    TXT = TXT + "\n\t- " + component['type']
+    TXT = TXT + "\n - " + component['type']
     for sub1 in SCHEMA.subtypes(component['type']):
-        TXT = TXT + "\n\t\t- " + sub1['type']
+        TXT = TXT + "\n. - " + sub1['type']
         for sub2 in SCHEMA.subtypes(sub1['type']):
-            TXT = TXT + "\n\t\t\t- " + sub2['type']
+            TXT = TXT + "\n   - " + sub2['type']
             for sub3 in SCHEMA.subtypes(sub2['type']):
-                TXT = TXT + "\n\t\t\t\t- " + sub3['type']
+                TXT = TXT + "\n    - " + sub3['type']
                 for sub4 in SCHEMA.subtypes(sub3['type']):
-                    TXT = TXT + "\n\t\t\t\t\t- " + sub4['type']
+                    TXT = TXT + "\n     - " + sub4['type']
 TXT = TXT + """
 
 Component types:
 """
 # List of component types
 for component in SCHEMA.components():
-    TXT = TXT + "\n\t- " + component['type']
+    TXT = TXT + "\n - " + component['type']
     for sub1 in SCHEMA.subtypes(component['type']):
-        TXT = TXT + "\n\t\t- " + sub1['type']
+        TXT = TXT + "\n  - " + sub1['type']
         for sub2 in SCHEMA.subtypes(sub1['type']):
-            TXT = TXT + "\n\t\t\t- " + sub2['type']
+            TXT = TXT + "\n   - " + sub2['type']
             for sub3 in SCHEMA.subtypes(sub2['type']):
-                TXT = TXT + "\n\t\t\t\t- " + sub3['type']
+                TXT = TXT + "\n    - " + sub3['type']
                 for sub4 in SCHEMA.subtypes(sub3['type']):
-                    TXT = TXT + "\n\t\t\t\t\t- " + sub4['type']
+                    TXT = TXT + "\n      - " + sub4['type']
  
 # List of terms to annotate containers
 TXT = TXT + """
@@ -60,11 +60,11 @@ TXT = TXT + """
 ### Terms for Containers
 """
 for term in SCHEMA.termsFor('Container'):
-    TXT = TXT + "\n\t- " + term['term']
+    TXT = TXT + "\n - " + term['term']
     for sub1 in SCHEMA.subterms(term['term']):
-        TXT = TXT + "\n\t\t- " + sub1['term']
+        TXT = TXT + "\n  - " + sub1['term']
         for sub2 in SCHEMA.subterms(sub1['term']):
-            TXT = TXT + "\n\t\t\t- " + sub2['term']
+            TXT = TXT + "\n   - " + sub2['term']
     
 
 
@@ -74,11 +74,11 @@ TXT = TXT + """
 
 """
 for term in SCHEMA.termsFor('Component'):
-    TXT = TXT + "\n\t- " + term['term']
+    TXT = TXT + "\n - " + term['term']
     for sub1 in SCHEMA.subterms(term['term']):
-        TXT = TXT + "\n\t\t- " + sub1['term']
+        TXT = TXT + "\n  - " + sub1['term']
         for sub2 in SCHEMA.subterms(sub1['term']):
-            TXT = TXT + "\n\t\t\t- " + sub2['term']
+            TXT = TXT + "\n    - " + sub2['term']
 
 TXT = TXT + """
 
@@ -94,7 +94,7 @@ for term in SCHEMA.terms():
             TXT = TXT + "*" + k + "*: " + str(term[k]) + "\n"
     if 'description' in term:
         TXT = TXT + "\n" + str(term[ 'description']) + "\n"
-    if 'example-values' in term:
+    if 'example-values' in term and term['example-values'] != '':
         TXT = TXT + "\n\n```" + str(term[ 'example-values']) + "\n```\n\n"
 
 with open(output_readme, "w") as text_file:
