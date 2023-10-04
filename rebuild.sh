4adefb50e8b19129c025dd98fc04e43a84d43ffe
Enrico Daga <enrico.daga@open.ac.uk>
2023-10-04 15:53:51 +0100

    Moved to script from reeco-python lib

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 source .python3/bin/activate
-git add generate-* reeco.py
-python3 generate-schema.py && python3 generate-readme.py && git add schema/ && git commit -m "Update" && git push
+python3 generate-readme.py && git add schema/ && git commit -m "Update" && git push
 
